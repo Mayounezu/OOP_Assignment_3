f3e45e11acdc27660fcc843b42bfdfae014a3d91
Mayounezu <roeyadar2007@gmail.com>
2026-06-23 16:06:23 +0300

    added the getters and setters for Unit class

--- Unit.java
+++ Unit.java
@@ -1,3 +1,47 @@
 public class Unit{
-    
+    private String name;
+    private int healthPool;
+    private int healthAmount;
+    private int atkPts;
+    private int defPts;
+
+    public int getAtkPts() {
+        return atkPts;
+    }
+
+    public int getHealthAmount() {
+        return healthAmount;
+    }
+
+    public int getHealthPool() {
+        return healthPool;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDefPts() {
+        return defPts;
+    }
+
+    public void setAtkPts(int atkPts) {
+        this.atkPts = atkPts;
+    }
+
+    public void setDefPts(int defPts) {
+        this.defPts = defPts;
+    }
+
+    public void setHealthAmount(int healthAmount) {
+        this.healthAmount = healthAmount;
+    }
+
+    public void setHealthPool(int healthPool) {
+        this.healthPool = healthPool;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
