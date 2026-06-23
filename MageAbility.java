7906df2633bd5db1d02760bd66c6da833c84b6e5
zivzivo <zivnurie@post.bgu.ac.il>
2026-06-23 18:38:15 +0300

    added Mage and MageAbility classes

---
+++ MageAbility.java
@@ -0,0 +1,17 @@
+public class MageAbility implements Ability {
+    private int hitCount;
+    private int spellPower;
+    private int range;
+    public MageAbility(int hitCount, int spellPower, int range) {
+        this.hitCount = hitCount;
+        this.spellPower = spellPower;
+        this.range = range;
+    }
+    public void cast(){
+        int hits = 0;
+        while (hits < this.hitCount && ) {
+            //hit enemy in range
+            hits++;
+        }
+    }
+}
