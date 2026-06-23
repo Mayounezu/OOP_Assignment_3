8c80c3caa39b80c977854cca6f100ff1d7ce78c9
Mayounezu <roeyadar2007@gmail.com>
2026-06-23 17:33:28 +0300

    added a postition class, the GameBoard is not correct yet

---
+++ Position.java
@@ -0,0 +1,30 @@
+public class Position {
+    private int x;
+    private int y;
+
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        if (x < 0) {
+            throw new IllegalArgumentException("X coordinate cannot be negative");
+        }
+        this.x = x;
+    }
+    public void setY(int y) {
+        if (y < 0) {
+            throw new IllegalArgumentException("Y coordinate cannot be negative");
+        }
+        this.y = y;
+    }
+}
