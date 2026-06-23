d90725841380acfa69e8a6d14e60501eba28bc27
Mayounezu <roeyadar2007@gmail.com>
2026-06-23 19:18:21 +0300

    got unit pullled

--- Unit.java
+++ Unit.java
@@ -8,44 +8,27 @@ public abstract class Unit{
 
     
 protected Unit(String name, int healthPool, int atkPts, int defPts, Position position){
+    this.name = name;
+    this.healthAmount = healthPool;
+    if (healthPool <= 0) {
+    this.healthPool = 1;
+    } else {
+    this.healthPool = healthPool;
+    }
+    this.atkPts = atkPts;
+    this.defPts = defPts;
+    this.position = position;
+    }
 
-this.name = name;
-
-this.healthAmount = healthPool;
-
-if (healthPool <= 0) {
-
-this.healthPool = 1;
-
-} else {
-
-this.healthPool = healthPool;
-
-}
-
-this.atkPts = atkPts;
-
-this.defPts = defPts;
-
-this.position = position;
-
-}
-
-public Position getPosition() {
-
-return position;
-
-}
+    public Position getPosition() {
+        return position;
+    }
 
 
 
-public void setPosition(Position p) {
-
-this.position = p;
-
-}
-
-
+    public void setPosition(Position p) {
+        this.position = p;
+    }
 
     public int getAtkPts() {
         return atkPts;
@@ -93,16 +76,29 @@ this.position = p;
         }
     }
 
-    public void visit(Floor floor){
-        if (floor.getOccupant() == null) {
-            floor.setOccupant(this);
-        } else {
-            throw new IllegalStateException("Cannot place a unit on a non-empty floor");
-        }
-    }
     public void visit(Wall wall){
         throw new IllegalStateException("Cannot place a unit on a wall");
     }
     
     public abstract void updateGameTick();
+
+    public void visit(Floor floor){
+        if (floor.getOccupant() == null) {
+            floor.setOccupant(this);
+        } else {
+            floor.getOccupant().accept(this);
+        }
+    }
+
+    public void visit(Player player){
+
+    }
+
+    public void startBattle(Unit enemy){
+        int damage = this.atkPts - enemy.getDefPts();
+        if (damage < 0) {
+            damage = 0;
+        }
+        enemy.setHealthAmount(enemy.getHealthAmount() - damage);
+    }
 }
